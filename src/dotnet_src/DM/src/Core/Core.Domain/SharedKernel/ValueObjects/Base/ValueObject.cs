9895c8920d90b64db113488798f0749fd3b1c503
valentin <germanov.ve@gmail.com>
2025-08-21 12:13:11 +0300

    add draft ValueObject

--- src/dotnet_src/DM/src/Core/Core.Domain/SharedKernel/ValueObjects/Base/ValueObject.cs
+++ src/dotnet_src/DM/src/Core/Core.Domain/SharedKernel/ValueObjects/Base/ValueObject.cs
@@ -2,7 +2,27 @@ using Core.Domain.SharedKernel.Abstractions;
 
 namespace Core.Domain.SharedKernel.ValueObjects.Base;
 
-public class ValueObject : IValueObject
+public abstract class ValueObject : IValueObject
 {
+    protected abstract IEnumerable<object> GetEqualityComponents();
+
+    public override bool Equals(object? obj) =>
+        GetType() == obj?.GetType()
+        &&
+        GetEqualityComponents().SequenceEqual(((ValueObject)obj).GetEqualityComponents());
     
+    
+    public override int GetHashCode() =>
+       GetEqualityComponents()
+           .Select(x => x?.GetHashCode() ?? 0)  
+           .Aggregate((x, y) => x ^ y);
+    
+    
+    public ValueObject? GetCopy() => MemberwiseClone() as ValueObject;
+    
+    public static bool operator ==(ValueObject? left, ValueObject? right) =>
+    // (left?.Equals(right)).GetValueOrDefault(); // TODO: BAD VARIANT null != null
+    //  left is null == right is null && (left is null || left.Equals(right));
+    Equals(left, right);
+    public static bool operator !=(ValueObject? left, ValueObject? right) => !(left == right);
 }
