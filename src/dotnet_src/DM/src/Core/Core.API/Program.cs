3f6f8fb27187725448ce054a67f2299c236c0c6c
valentin <germanov.ve@gmail.com>
2025-09-01 14:23:47 +0300

    upd Program

--- src/dotnet_src/DM/src/Core/Core.API/Program.cs
+++ src/dotnet_src/DM/src/Core/Core.API/Program.cs
@@ -2,6 +2,8 @@ using System.Reflection;
 using Core.API.Extensions.DI;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer(); // Minimal API
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddRepositories(builder.Configuration);
@@ -10,10 +12,27 @@ builder.Services.AddEventHandlers();
 var app = builder.Build();
 
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapGet("/", () => "Hello World!");
 
-var apiVersion = app.MapGroup("/api/v1");
+var apiVersion = app.MapGroup("/api/v1")
+    .WithOpenApi(operation =>
+    {
+        operation.Responses.TryAdd("400", new Microsoft.OpenApi.Models.OpenApiResponse { Description = "Request validation error" });
+        operation.Responses.TryAdd("422", new Microsoft.OpenApi.Models.OpenApiResponse { Description = "Validation error" });
+        operation.Responses.TryAdd("404", new Microsoft.OpenApi.Models.OpenApiResponse { Description = "Resource not found" });
+        operation.Responses.TryAdd("409", new Microsoft.OpenApi.Models.OpenApiResponse { Description = "Conflict" });
+        operation.Responses.TryAdd("403", new Microsoft.OpenApi.Models.OpenApiResponse { Description = "Forbidden" });
+        operation.Responses.TryAdd("500", new Microsoft.OpenApi.Models.OpenApiResponse { Description = "Unexpected server error" });
+        
+        
+        return operation;
+    });
 app.AddMapsConfigure(apiVersion, [Assembly.GetExecutingAssembly()]);
 
 app.Run();
